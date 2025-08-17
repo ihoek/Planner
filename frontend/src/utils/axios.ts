b2d374b4e38586024d404a40a4f7a8a91c71559e
ihoek <ihoek679@naver.com>
2025-08-18 00:19:50 +0900

    fix : axios 인스턴스 생성

---
+++ frontend/src/utils/axios.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// Axios 인스턴스 생성
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
